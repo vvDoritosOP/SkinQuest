f9fe2e503c1cc23e47fa6221b8cfd4b33d17c36b
Daniel <dorito315f@gmail.com>
2026-03-25 12:19:57 -0700

    face model oval on screen

--- app/facemodel.tsx
+++ app/facemodel.tsx
@@ -8,11 +8,26 @@ export default function FaceModel() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#d2b48c" }}>
       <Text style={{ fontSize: 24, color: "#b0e0e6", marginBottom: 20 }}>Your Enemies</Text>
+      
+      
+      
+      <View style={{
+    backgroundColor: "#F1C27D",
+    height: 450,
+    width: 400,
+    borderRadius:125,
+
+
+    }}></View>
+      
+      
       {concerns.map((name, index) => (
-        <Text key={index} style={{ fontSize: 20, color: "white", padding: 10 }}>
+        <Text key={index} style={{ fontSize: 20, color: "red", padding: 10 }}>
           {name}
         </Text>
       ))}
     </View>
   );
+   
+
 }
