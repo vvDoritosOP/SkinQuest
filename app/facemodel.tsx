1a5cfa2d3cda76ed09e12801addd81958f093e3f
Daniel <dorito315f@gmail.com>
2026-03-25 15:54:38 -0700

    face model with eyes, mouth and acne enemies

--- app/facemodel.tsx
+++ app/facemodel.tsx
@@ -13,12 +13,81 @@ export default function FaceModel() {
       
       <View style={{
     backgroundColor: "#F1C27D",
-    height: 450,
-    width: 400,
+    height: 300,
+    width: 250,
     borderRadius:125,
+     }}>
+    
+    <View style = {{flexDirection: "row", justifyContent: "center", gap: 40, marginTop: 100}}> 
+    <View style={{ 
+  backgroundColor: "white",
+  width: 30,
+  height:30,
+  borderRadius: 50,
+  alignItems: "center",
+  justifyContent: "center",
+    }}> 
+      <View style={{ backgroundColor: "#3B1F0B", width: 15, height: 15, borderRadius: 8 }}></View>
+    </View>
+
+    <View style={{
+  backgroundColor:"white",
+  width:30,
+  height:30,
+  borderRadius:50,
+  alignItems:"center",
+  justifyContent:"center",
+
+}}>
+    <View style={{ backgroundColor: "#3B1F0B", width: 15, height: 15, borderRadius: 8 }}></View>
+</View>
+  
+</View>
+
+<View style={{
+backgroundColor: "#D4736E",
+  width: 40,
+  height: 15,
+  borderRadius: 10,
+  alignSelf: "center",
+  marginTop: 50,
+
+}}></View>
+  <View style={{
+    position: "absolute",
+    top: 50,
+    left: 100,
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    backgroundColor: "red",
 
 
-    }}></View>
+  }}>
+</View>
+<View style={{
+  position: "absolute",
+  bottom: 60,
+  right: 80,
+  width: 12,
+  height: 12,
+  borderRadius: 6,
+  backgroundColor: "red",
+
+}}>
+</View>
+
+<View style={{
+position: "absolute",
+bottom: 80,
+left: 70,
+width: 12,
+height: 12,
+borderRadius: 6,
+backgroundColor: "red",
+
+}}></View>
+  </View>
       
       
       {concerns.map((name, index) => (
