8eb6c216f0268d6199776e2960e63e11073a59db
Daniel <dorito315f@gmail.com>
2026-04-16 22:20:32 -0700

    styled concerns screen with custom backgrounds

--- app/concerns.tsx
+++ app/concerns.tsx
@@ -1,18 +1,18 @@
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Concerns() {
   const router = useRouter();
   
   const [problems, setProblems] = useState([
 
-    { id: 1, text: `Acne`, selected: false},
-    { id: 2, text: `Blackheads`, selected: false},
-    { id: 3, text: `Wrinkles`, selected: false},
-    { id: 4, text: `Redness`, selected: false},
-    { id: 5, text: `Eye Bags`, selected: false},
-    { id: 6, text: `Oiliness`, selected: false},
+    { id: 1, text: `Acne`, selected: false, image: require("@/assets/acneback.png"), textColor: "#ee6631"},
+    { id: 2, text: `Blackheads`, selected: false, image: require("@/assets/blackback.png") , textColor: "#4e4d4d"},
+    { id: 3, text: `Wrinkles`, selected: false, image: require("@/assets/wrinkback.png"), textColor: "#9866af"},
+    { id: 4, text: `Redness`, selected: false,  image: require("@/assets/redback.png"), textColor: "#cf5858"},
+    { id: 5, text: `Eye Bags`, selected: false,  image: require("@/assets/eyeback.png"), textColor: "#4c2496"},
+    { id: 6, text: `Oiliness`, selected: false,  image: require("@/assets/oilback.png"), textColor: "#bd931f"},
   ]);
 
   function toggleConcerns(id){
@@ -38,19 +38,33 @@ export default function Concerns() {
 
   return (
 
-    <View style={{ backgroundColor:"#d2b48c", flex: 1, alignItems: "center", justifyContent: "space-between" , paddingVertical:50}}>
+    <ImageBackground 
+        source={require("@/assets/background.png")}
+        style={styles.container}
+        resizeMode="cover"
+        > 
        <Text style={{ fontSize: 24, color: "#b0e0e6", fontWeight:"bold" }}>Select what you want to defeat!</Text>
        
        {problems.map((item) => (
         <TouchableOpacity 
         key={item.id} 
         onPress={() => toggleConcerns(item.id)}
-        style = {[styles.card,  {backgroundColor: item.selected ? "green" : "#e9967a", 
+        style = {[styles.card,  {backgroundColor: item.selected ? "#000000" : "transparent"
          }]}
         >
-        <Text style={{ fontSize: 20, color: "gold", padding: 5}}>
-        {item.text}
-        </Text>
+        <ImageBackground
+      source={item.image}
+      style={styles.cardImage}
+      imageStyle={{ borderRadius: 10, top: 10, height: '120%', width:'120%', left: '-10%' }}
+      resizeMode="cover"
+    >
+        <View style={[styles.overlay, { backgroundColor: item.selected ? 'rgba(0,128,0,0.5)' : 'rgba(0, 0, 0, 0)' }]}>
+        <Text style={{ 
+        fontSize: 20, 
+        fontWeight: "bold", 
+        color: item.selected ? "white" : item.textColor }}>{item.text}</Text>
+      </View>
+        </ImageBackground>
         </TouchableOpacity>
        ))}
        <Text style={{color:"#b0e0e6", fontWeight: "bold"}}>{problems.filter((item)=>item.selected).length}/3 Selected </Text>
@@ -83,24 +97,38 @@ export default function Concerns() {
        </TouchableOpacity>
 
 
-      </View>
+      </ImageBackground>
   );
  
   
 }
 
  
-  const styles = StyleSheet.create({
-    card:{
-      padding : 15,
-      margin : 5,
-      borderRadius : 10,
-      borderWidth : 0,
-      width: "80%",
-      alignItems: "center",  
-      
-      
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    margin: 5,
+    borderRadius: 10,
+    width: "80%",
+    height: 110, 
+    overflow: "hidden", 
+  },
+  cardImage: {
+    width: '100%',
+  height: '100%',
+  justifyContent: "center",
+  alignItems: "center",
+  },
+  overlay: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
 
-    }
-
-  });
+  
